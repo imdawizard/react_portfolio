1cb435dda2d8545dff521a721fa080aea9b9bbb7
Spyrodragonman Mannyman <nate.is.cool.yeah@gmail.com>
2023-10-03 19:21:27 -0600

    changed hashrouter to browserrouter

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
 import React from 'react';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/header';
 import AboutMe from './pages/AboutMe.js';
 import Portfolio from './pages/Portfolio';
